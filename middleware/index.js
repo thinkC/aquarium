830b3002ea1dbe358beda8f79a18667dc5fe6f18
Tunde Oyewo <tunde.oyewo@gmail.com>
2017-12-28 19:57:44 +0000

    add new portfolio site

---
+++ middleware/index.js
@@ -0,0 +1,61 @@
+var Portfolio = require("../models/portfolio");
+var Comment = require("../models/comment");
+
+//All Middleware
+var middlewareObj = {
+    
+    isLoggedIn : function(req, res, next){
+        if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "Please login first")
+    res.redirect("/login");
+    },
+    
+    checkPortfolioOwnership : function(req, res, next){
+        if(req.isAuthenticated()){
+            Portfolio.findById(req.params.id, function(err, foundPortfolio){
+            console.log(foundPortfolio)
+        if(err){
+            req.flash("error", "Portfolio not found")
+            res.redirect("back");
+        }else{
+            //does user own the portfolio?
+            if(foundPortfolio.author.id.equals(req.user._id)){
+                next();
+            }else{
+                req.flash("error", "You do not have permission");
+                res.redirect("back");
+            }
+            
+        }
+    })
+    }else{
+        req.flash("error", "You need to be logged in first");
+        res.redirect("back")
+    }
+    },
+    
+    checkCommentOwnership : function(req, res, next){
+         if(req.isAuthenticated()){
+            Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err){
+                req.flash("error", "Comment not found")
+                res.redirect("back");
+            }else{
+                if(foundComment.author.id.equals(req.user._id)){
+                    next()
+                }else{
+                    req.flash("error", "You do not have permission")
+                    res.redirect("back")
+                }
+            }
+        })
+    }else{
+        req.flash("error", "You need to be logged in first");
+        res.redirect("back");
+    }
+    }
+}
+
+module.exports = middlewareObj
